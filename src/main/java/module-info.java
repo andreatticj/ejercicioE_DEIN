5b0e0f9e862927ef873588261253985152819b5b
andreatticj <andreatticj@gmail.com>
2024-10-17 13:41:01 +0200

    CHANGE ejercicioE.fxml , modalE.fxml y module-info.java para la corresponencia de nombre con el proyecto, daba fallo al ejecutar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,12 @@
-module eu.andreatt.ejerciciod_dein {
+module eu.andreatt.ejercicioe_dein {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-//    opens eu.andreatt.ejerciciod_dein to javafx.fxml;
-//    exports eu.andreatt.ejerciciod_dein;
-    exports eu.andreatt.ejerciciod_dein.controller;
-    opens eu.andreatt.ejerciciod_dein.controller to javafx.fxml;
-    exports eu.andreatt.ejerciciod_dein.application;
-    opens eu.andreatt.ejerciciod_dein.application to javafx.fxml;
+//    opens eu.andreatt.ejercicioe_dein to javafx.fxml;
+//    exports eu.andreatt.ejercicioe_dein;
+    exports eu.andreatt.ejercicioe_dein.controller;
+    opens eu.andreatt.ejercicioe_dein.controller to javafx.fxml;
+    exports eu.andreatt.ejercicioe_dein.application;
+    opens eu.andreatt.ejercicioe_dein.application to javafx.fxml;
 }
